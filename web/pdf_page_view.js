e774e4dcaa5f344ead754786fd132cf4bb835b64
Martynas Bagdonas <git.martynas@gmail.com>
2023-11-01 19:05:38 +0000

    Expose not rounded canvas width
    (cherry picked from commit 114da0e12e338c9202aa8358209d9f9cc9390e70)

--- web/pdf_page_view.js
+++ web/pdf_page_view.js
@@ -1006,6 +1006,7 @@ class PDFPageView {
         this.#hasRestrictedScaling = false;
       }
     }
+    this.currentCanvasWidth = width * outputScale.sx;
     const sfx = approximateFraction(outputScale.sx);
     const sfy = approximateFraction(outputScale.sy);
 
